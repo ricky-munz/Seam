96c3e11550ae7990ce99c94e22deb0ac145f519d
Ricky Munz <ricky.munz@gmail.com>
2024-03-22 20:09:54 -0400

    Replace default with view controller unit tests.

---
+++ SeamTests/ViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerTests.swift
+//  SeamTests
+//
+//  Created by Ricky Munz on 3/22/24.
+//
+
+import XCTest
+@testable import Seam
+
+final class ViewControllerTests: XCTestCase {
+
+}
